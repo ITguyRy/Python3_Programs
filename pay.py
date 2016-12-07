88b487994ce015feaa330beac66c089f326eee9b
Ryan Ung <rykung@gmail.com>
2016-12-06 23:32:55 -0800

    modified:   pay.py

--- pay.py
+++ pay.py
@@ -3,13 +3,12 @@ hours = int(input("How many hours did you work this week? "))
 if hours > 40:
     overhour = hours - 40
     print('you worked {} hours overtime'.format(overhour))
+else:
+    overhour = 0
+    print('you did not work overtime')
 
 
 wage = float(input("What is your hourly wage? "))
-
-
-print(overhour)
-
 
 
 
