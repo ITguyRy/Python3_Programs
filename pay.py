626040e84344c9d1ef9c4f6d3bd094af5d63f431
ITguyRy <rykung@gmail.com>
2016-12-06 23:30:24 -0800

    Add files via upload

--- pay.py
+++ pay.py
@@ -3,32 +3,38 @@ hours = int(input("How many hours did you work this week? "))
 if hours > 40:
     overhour = hours - 40
     print('you worked {} hours overtime'.format(overhour))
+elif hours < 0:
+    overhour= 0
+    print("you did not even work!" )
+else:
+    overhour= 0
+    print('you did not work overtime buddy! ')
+
+if hours > 0:
+    wage = float(input("What is your hourly wage? "))
 
 
-wage = float(input("What is your hourly wage? "))
-
-
-print(overhour)
 
 
 
 
 
- 
-
-def weeklypay(hours,wage,overhour) :
-    
-    overtime = overhour *(1.5 * wage)
-
-    base = hours * wage
-    
-    amount = base + overtime
-    print ("Weekly base earned: {}".format(base))
-    print ("Overtime money: {}".format(overtime))
-    print ("Total week earned: {}".format(amount))
 
 
-weeklypay(hours, wage,overhour)
+
+    def weeklypay(hours,wage,overhour) :
+
+        overtime = overhour *(1.5 * wage)
+
+        base = hours * wage
+
+        amount = base + overtime
+        print ("Weekly base earned: {}".format(base))
+        print ("Overtime money: {}".format(overtime))
+        print ("Total week earned: {}".format(amount))
+
+
+    weeklypay(hours, wage,overhour)
 
 
 
